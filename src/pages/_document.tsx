b05b152d668ffc1e99282c3f4f390df5e162a8b1
Jaine Scruph <jscruph10@gmail.com>
2022-07-01 10:57:59 -0300

    Editing link font family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,7 +33,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
             rel="stylesheet"
           />
           <link
