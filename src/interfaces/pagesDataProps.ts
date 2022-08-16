16b84414c9edae6ab9d1f45aaa774a9b4c9b8a94
Jaine Scruph <jscruph10@gmail.com>
2022-08-16 12:12:34 -0300

    Integration of events page

--- src/interfaces/pagesDataProps.ts
+++ src/interfaces/pagesDataProps.ts
@@ -1,8 +1,11 @@
+import { Events } from './events';
 import { News, DataNewsContent } from './news';
 import { Advertisement } from './advertisement';
 
 export interface pagesDataProps {
   news: News[];
+
+  events: Events[];
 
   recentNews: News[];
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextSeo } from 'next-seo';
 import type { GetStaticProps, NextPage } from 'next';
 
+import { getEvents } from '~/services/functions/getEvents';
 import { getRecentNews } from '~/services/functions/getRecentNews';
 import { getAdvertisement } from '~/services/functions/getAdvertisement';
 
@@ -18,7 +19,11 @@ import * as S from '~/styles/pages';
 
 import { pagesDataProps } from '~/interfaces/pagesDataProps';
 
-const Home: NextPage = ({ advertisement, recentNews }: pagesDataProps) => {
+const Home: NextPage = ({
+  advertisement,
+  recentNews,
+  events,
+}: pagesDataProps) => {
   return (
     <S.Container>
       <NextSeo
@@ -30,7 +35,7 @@ const Home: NextPage = ({ advertisement, recentNews }: pagesDataProps) => {
         <Header />
         <Channels />
         <News data={recentNews} amount_of_news={4} />
-        <Events />
+        <Events data={events} />
         <Sponsors data={advertisement} />
       </S.Wrapper>
       <Footer />
@@ -41,11 +46,13 @@ const Home: NextPage = ({ advertisement, recentNews }: pagesDataProps) => {
 export default Home;
 
 export const getStaticProps: GetStaticProps = async () => {
+  const events = await getEvents();
   const recentNews = await getRecentNews();
   const advertisement = await getAdvertisement();
 
   return {
     props: {
+      events: events ? events : [],
       recentNews: recentNews ? recentNews : [],
       advertisement: advertisement ? advertisement : [],
     },
